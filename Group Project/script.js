6c7f3ed475a0899fc395c25716f4664b582f2267
HesamPourabbasian <137700266+HesamPourabbasian@users.noreply.github.com>
2024-05-17 21:42:30 +0330

    Add files via upload

---
+++ Group Project/script.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const backgroundDiv = document.getElementById('gif');
+    const toggleButton = document.getElementById('changeButton');
+    let isDefaultBackground = true;
+
+    toggleButton.addEventListener('click', () => {
+        if (isDefaultBackground) {
+            backgroundDiv.style.backgroundImage = 'url("./img/gif2.gif")'; // Alternate background image
+        } else {
+            backgroundDiv.style.backgroundImage = 'url("./img/gif.gif")'; // Reset to default background image
+        }
+        isDefaultBackground = !isDefaultBackground;
+    });
+});
+
+
+
+
+
+
+
+
+
+// br
+var icon = document.getElementById ("changeButton");
+icon.onclick = function() {
+    document.body.classList.toggle("dark-theme");
+
+if ( document.body.classList.contains("dark-theme")) {
+    icon.src = "./svg/dark.svg";
+} else{
+    icon.src ="./svg/light_mode.svg";
+}
+}
+// br 
+
+
