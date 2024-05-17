919485ca088f918bea1b4c52042343aae76b393f
HesamPourabbasian <137700266+HesamPourabbasian@users.noreply.github.com>
2024-05-17 18:21:50 +0330

    Delete Group Project directory

--- Group Project/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('fileInput').addEventListener('change', function() {
-    const selectedFile = document.getElementById('fileInput').files[0];
-    console.log('Selected file:', selectedFile);
-});
